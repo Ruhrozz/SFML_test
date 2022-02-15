e523ac67861443b7f495369e8d98a7f5257a55f5
catratJAM <y.timofeeva1@g.nsu.ru>
2022-02-15 10:01:48 +0700

    Added not working code for testing.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <SFML/Window.hpp>
+
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    sf::Window window(sf::VideoMode(800, 600), "My window");
     return 0;
 }

275bf6abe77f45a8e2a2a6c79adf0efa0b76d971
Ruhrozz <ruhrozz@bk.ru>
2022-02-17 23:16:48 +0700

    Added TODO Added getButton function Added Fullscreen button Moved drawing into while loop

--- main.cpp
+++ main.cpp
@@ -2,39 +2,36 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+
+sf::RectangleShape *getButton(float width, float height, float x, float y) {
+    auto file_button = new sf::RectangleShape(sf::Vector2f(width, height));
+    file_button->setFillColor(sf::Color(140, 140, 140));   //158, 158, 158
+    file_button->setPosition(sf::Vector2f(x, y));
+    return file_button;
+}
+
+
 int main()
 {
+    bool isFullscreen = false;
+
+
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
-    //--------------------INTERFACE--------------------//
-    //(BUTTONS AND MAYBE STH ELSE)
-    sf::RectangleShape file_button(sf::Vector2f(50, 30));
-    file_button.setFillColor(sf::Color(140, 140, 140));   //158, 158, 158
-    file_button.setPosition(sf::Vector2f(0, 0));
 
-    sf::RectangleShape edit_button(sf::Vector2f(50, 30));
-    edit_button.setFillColor(sf::Color(140, 140, 140));
-    edit_button.setPosition(sf::Vector2f(52, 0));
-
-    sf::RectangleShape brush_button(sf::Vector2f(80, 160));
-    brush_button.setFillColor(sf::Color(140, 140, 140));
-    brush_button.setPosition(sf::Vector2f(50, 40));
-    sf::Vector2i mouse = sf::Mouse::getPosition(window);            //vector input mouse' position in window
+    //TODO: create shapes and pictures array
+    auto file_button = getButton(50, 30, 0, 0);
+    auto edit_button = getButton(50, 30, 52, 0);
+    auto brush_button = getButton(300, 300, 50, 40);
 
 
-    window.clear(sf::Color(111, 111, 111));
-    //--------------------ACTING--------------------------//
-    //it doesn't work, need to fix it
-    if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
-        if(file_button.getGlobalBounds().contains(mouse.x, mouse.y))
-        {
-            std::cout << "AAAAAAAAAAAAAAAa\n";
-        }
     //--------------------STRINGS-------------------------//
     sf::Font font;
     std::string path("..\\fonts\\ArialRegular.ttf");
     font.loadFromFile(path);
 
+
+    // TODO: !squeeze to functions!
     sf::Text file_text;
     file_text.setFont(font);
     file_text.setCharacterSize(15);
@@ -46,6 +43,8 @@ int main()
     edit_text.setCharacterSize(15);
     edit_text.setString("Edit");
     edit_text.setPosition(sf::Vector2f(65, 7));
+
+
     //--------------------PICTURES---------------------//
     //(FOR BRUSH, ERASER, COLORS, ETC.)
 
@@ -57,15 +56,9 @@ int main()
 //    test_image.setPosition(0, 500);
 
     //--------------------DRAWING----------------------//
-    window.draw(file_button);
-    window.draw(edit_button);
-    window.draw(brush_button);
-    window.draw(file_text);
-    window.draw(edit_text);
-//    window.draw(test_image);
-    window.display();
 
-    while (window.isOpen()) // run the program as long as the window is open
+
+    while (window.isOpen())
     {
         sf::Event event{};
         while (window.pollEvent(event))
@@ -73,6 +66,38 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::F5))
+        {
+            window.close();
+            if(!isFullscreen)
+                window.create(sf::VideoMode::getDesktopMode(), "My window", sf::Style::Fullscreen);
+            else
+                window.create(sf::VideoMode(800, 600), "My window");
+            isFullscreen = !isFullscreen;
+            while(sf::Keyboard::isKeyPressed(sf::Keyboard::F5))
+                ;
+        }
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            sf::Vector2i mouse = sf::Mouse::getPosition(window);
+            if (file_button->getGlobalBounds().contains(mouse.x, mouse.y))
+            {
+                std::cout << "AAAAAAAAAAAAAAAa" << std::endl;
+            }
+        }
+
+        window.clear(sf::Color(111, 111, 111));
+
+        window.draw(*file_button);
+        window.draw(*edit_button);
+        window.draw(*brush_button);
+        window.draw(file_text);
+        window.draw(edit_text);
+//        window.draw(test_image);
+
+        window.display();
     }
     return 0;
 }

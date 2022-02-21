ad4cdf4ed07efeae787abda4a48d217d86f7843e
catratJAM <y.timofeeva1@g.nsu.ru>
2022-02-21 13:41:34 +0700

    Removed window's titlebar Added quit button Added arrays for buttons and texts Added cycles

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-
 sf::RectangleShape *getButton(float width, float height, float x, float y) {
-    auto file_button = new sf::RectangleShape(sf::Vector2f(width, height));
-    file_button->setFillColor(sf::Color(140, 140, 140));   //158, 158, 158
-    file_button->setPosition(sf::Vector2f(x, y));
-    return file_button;
+    auto button = new sf::RectangleShape(sf::Vector2f(width, height));
+    button->setFillColor(sf::Color(140, 140, 140));   //158, 158, 158
+    button->setPosition(sf::Vector2f(x, y));
+    return button;
+}
+
+sf::Text *getText(const sf::Font& font, const sf::String &str, float x, float y, unsigned int CharacterSize = 15)
+{
+    auto text = new sf::Text;
+    text->setFont(font);
+    text->setCharacterSize(CharacterSize);
+    text->setString(str);
+    text->setPosition(sf::Vector2f(x, y));
+    return text;
 }
 
 
@@ -15,36 +24,25 @@ int main()
 {
     bool isFullscreen = false;
 
-
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-
+    //todo::create button for resize
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "My window", sf::Style::None);
+    window.setVerticalSyncEnabled(true);
 
     //TODO: create shapes and pictures array
     auto file_button = getButton(50, 30, 0, 0);
     auto edit_button = getButton(50, 30, 52, 0);
     auto brush_button = getButton(300, 300, 50, 40);
+    auto quit_button = getButton(30, 30, 1024 - 30 , 0);
 
-
+    sf::RectangleShape * buttons[] = {file_button, edit_button, brush_button, quit_button};
     //--------------------STRINGS-------------------------//
     sf::Font font;
     std::string path("..\\fonts\\ArialRegular.ttf");
     font.loadFromFile(path);
 
-
-    // TODO: !squeeze to functions!
-    sf::Text file_text;
-    file_text.setFont(font);
-    file_text.setCharacterSize(15);
-    file_text.setString("File");
-    file_text.setPosition(sf::Vector2f(13, 7));
-
-    sf::Text edit_text;
-    edit_text.setFont(font);
-    edit_text.setCharacterSize(15);
-    edit_text.setString("Edit");
-    edit_text.setPosition(sf::Vector2f(65, 7));
-
-
+    auto file_text = getText(font, "File", 13, 7, 15);
+    auto edit_text = getText(font, "Edit", 65, 7, 15);
+    sf::Text * texts[] = {file_text, edit_text};
     //--------------------PICTURES---------------------//
     //(FOR BRUSH, ERASER, COLORS, ETC.)
 
@@ -71,14 +69,20 @@ int main()
         {
             window.close();
             if(!isFullscreen)
+            {
                 window.create(sf::VideoMode::getDesktopMode(), "My window", sf::Style::Fullscreen);
+                quit_button = getButton(30, 30, sf::VideoMode::getDesktopMode().width - 30, 0);
+            }
             else
-                window.create(sf::VideoMode(800, 600), "My window");
+            {
+                window.create(sf::VideoMode(1024, 768), "My window");
+                quit_button = getButton(30, 30, 1024 - 30 , 0);
+            }
             isFullscreen = !isFullscreen;
             while(sf::Keyboard::isKeyPressed(sf::Keyboard::F5))
                 ;
         }
-
+        //todo: maybe rewrite as "switch — case" construction or cycle? also maybe 77-83 apply to resize button
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
         {
             sf::Vector2i mouse = sf::Mouse::getPosition(window);
@@ -86,18 +90,20 @@ int main()
             {
                 std::cout << "AAAAAAAAAAAAAAAa" << std::endl;
             }
+            if (quit_button->getGlobalBounds().contains(mouse.x, mouse.y))
+            {
+                window.close();
+            }
         }
 
         window.clear(sf::Color(111, 111, 111));
+        for(auto & button : buttons) window.draw(*button);
+        for (auto & text : texts) window.draw(*text);
 
-        window.draw(*file_button);
-        window.draw(*edit_button);
-        window.draw(*brush_button);
-        window.draw(file_text);
-        window.draw(edit_text);
-//        window.draw(test_image);
+//      window.draw(test_image);
 
         window.display();
     }
     return 0;
 }
+
